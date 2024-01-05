f235ab4456cd48076b44101ca6493f1f72e521e6
wharry <devwharry@gmail.com>
2024-01-05 09:52:45 -0300

    Criando header e footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <span>React + TS Todo List</span> @ 2024.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
