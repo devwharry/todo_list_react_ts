4133ef889ab3472443649ac44cf27ab26aa3345c
wharry <devwharry@gmail.com>
2024-01-06 11:11:27 -0300

    Estilizando modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 // CSS
 import styles from './Modal.module.css';
 
-interface Props {}
+interface Props {
+    children: React.ReactNode;
+}
 
-const Modal = (props: Props) => {
+const Modal = ({children}: Props) => {
   return (
     <div id='modal'>
-        <div></div>
-        <div>
+        <div className={styles.fade}></div>
+        <div className={styles.modal}>
             <h2>Texto do Modal</h2>
+            {children}
         </div>
     </div>
   );
