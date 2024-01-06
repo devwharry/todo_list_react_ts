aa2c58062f2058b6412b97d219285049c1d25024
wharry <devwharry@gmail.com>
2024-01-06 10:58:40 -0300

    Criando Modal

---
+++ src/components/Modal.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// CSS
+import styles from './Modal.module.css';
+
+interface Props {}
+
+const Modal = (props: Props) => {
+  return (
+    <div id='modal'>
+        <div></div>
+        <div>
+            <h2>Texto do Modal</h2>
+        </div>
+    </div>
+  );
+};
+
+export default Modal;
