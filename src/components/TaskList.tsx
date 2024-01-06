d1400a9c7517a595f6ce9a66441d2872be4eb54f
wharry <devwharry@gmail.com>
2024-01-06 11:48:22 -0300

    Preenchendo modal com dados

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -9,7 +9,7 @@ import styles from './TaskList.module.css';
 interface Props {
   taskList: ITask[];
   handledelete(id: number): void;
-  handleEdit(): void;
+  handleEdit(task: ITask): void;
 }
 
 const TaskList = ({taskList, handledelete, handleEdit}: Props) => {
@@ -23,7 +23,7 @@ const TaskList = ({taskList, handledelete, handleEdit}: Props) => {
               <p>Dificuldade: {task.difficulty}</p>
             </div>
             <div className={styles.actions}>
-              <i className='bi bi-pencil' onClick={() => handleEdit()}></i>
+              <i className='bi bi-pencil' onClick={() => handleEdit(task)}></i>
               <i className='bi bi-trash' onClick={() => {handledelete(task.id)}}></i>
             </div>
           </div>
